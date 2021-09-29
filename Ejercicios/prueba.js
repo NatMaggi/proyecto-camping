e0a6e225f06701bae1f5b9e0f9a33f77c9a40aa6
NatMaggi <nn.maggi@gmail.com>
2021-09-29 11:58:39 -0300

    proyectos1

---
+++ Ejercicios/prueba.js
@@ -0,0 +1,74 @@
+/* EJERCICIO 1 - A
+
+var a = parseInt( prompt('ingrese un valor $'));
+console.log(a);
+
+var b = parseInt( prompt('ingrese un valor $'));
+console.log(b);
+
+var c= parseInt( prompt('ingrese un valor $'));
+console.log(c);
+
+var suma = alert('El total es ' + (a + b + c));
+*/
+
+/* EJERCICIO 1 -B
+
+var a = parseInt( prompt('ingrese un valor $'));
+console.log(a);
+
+var b = parseInt( prompt('ingrese un valor $'));
+console.log(b);
+
+var c= parseInt( prompt('ingrese un valor $'));
+console.log(c);
+
+var suma = alert('El total es ' + (a + b + c)/ 3);
+*/
+
+/* EJERCICIO 1 -C
+
+var a = parseInt( prompt('ingrese un valor $'));
+console.log(a);
+
+var b = parseInt( prompt('ingrese un valor $'));
+console.log(b);
+
+var c= parseInt( prompt('ingrese un valor $'));
+console.log(c);
+
+var suma = (a + b + c)
+var masIva = (suma) + (suma* 21 / 100);
+
+console.log(masIva);
+
+*/
+
+var auto = 100000;
+
+var ahorro = prompt("ingrese su saldo disponible");
+
+var edad = prompt("ingrese su edad");
+
+if(ahorro>=auto && edad>=18){
+    alert("Felicitaciones, puedes comprar el auto");
+} 
+else {
+    alert("lo siento, tu saldo no es suficiente");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ VIAJES/probando.js
@@ -0,0 +1,6 @@
+function ListadoPaises (p1,p2,...p){
+    console.log (p1);
+    console.log (p2);
+    console.log (p);
+}
+ListadoPaises ('españa', 'brasil', 'bolivia', 'peru' );
